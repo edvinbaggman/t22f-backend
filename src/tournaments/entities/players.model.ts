527924a20dd9870ea0077f9ceae8b41e78542295
LACHIRI Ilias <ilias.lachiri@grenoble-inp.org>
2023-10-16 10:42:43 +0200

    adding enum for Sex

--- src/tournaments/entities/players.model.ts
+++ src/tournaments/entities/players.model.ts
@@ -1,7 +1,9 @@
 import { Tournaments } from './tournaments.model';
+import { Sex } from '../enum/sex.enum';
 
 export class Players {
   id?: string;
   name: string;
+  sex: Sex;
   stats: Tournaments;
 }
