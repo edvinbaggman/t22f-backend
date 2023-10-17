e0b62b99f058fc5c67532d4ff3f00509bb99b86b
LACHIRI Ilias <ilias.lachiri@grenoble-inp.org>
2023-10-17 16:25:15 +0200

    deploy new errror 5: use of .env file

--- src/firebase/gcs.config.ts
+++ src/firebase/gcs.config.ts
@@ -2,11 +2,11 @@ import { Storage } from '@google-cloud/storage';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-console.log('Process GCP key : ' + process.env.GCP_KEY);
+console.log('Process GCP key : ' + process.env.GCP_KEY_LOCAL);
 
 const storage = new Storage({
   projectId: 'takes-two-to-fwango',
-  credentials: JSON.parse(process.env.GCP_KEY),
+  credentials: JSON.parse(process.env.GCP_KEY_LOCAL),
 });
 
 export const gcsBucketName = 'takes-two-to-fwango.appspot.com';
