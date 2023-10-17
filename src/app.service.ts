550492760eb7ba4c0feae650cfa701e90b5856df
LACHIRI Ilias <ilias.lachiri@grenoble-inp.org>
2023-10-17 18:04:01 +0200

    IT works

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'IT FUCKIIIING WORKIIIING !';
+    return 'NOT This part  The error :)!';
   }
 }
